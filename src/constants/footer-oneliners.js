1aee1e59d9c399d23dbcb68f5cd8fb63c15cd47f
Thibault Maekelbergh <thibault.maekelbergh@iCloud.com>
2017-11-22 12:57:11 +0100

    Remove oneliners from src/constants

--- src/constants/footer-oneliners.js
+++
@@ -1,9 +0,0 @@
-export default [
-  `🎔 LOVE 🎔`,
-  `a good amount of ristretto's`,
-  `a lot of lunchbreaks at my desk`,
-  `a Nina Kraviz set blasting through my headphones`,
-  `lots of awe for GraphQL`,
-  `lots of heart eyes for Contentful`,
-  `medium-to-heavy swearing`,
-];
