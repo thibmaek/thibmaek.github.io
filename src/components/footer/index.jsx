6ab3b372892cdb6e6f0ab5aa053f75b1e755094a
Thibault Maekelbergh <thibault.maekelbergh@iCloud.com>
2017-11-27 21:54:09 +0100

    Add contentful attribution

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -7,8 +7,17 @@ import './index.css';
 
 const Footer = ({ author, oneliners }) => (
   <footer className='main-footer'>
-    <p>Copyright &copy; {new Date().getFullYear()} {author} All Rights Reserved.</p>
-    <p>Made with {pickRandom(oneliners)}</p>
+    <div>
+      <p>Copyright &copy; {new Date().getFullYear()} {author} All Rights Reserved.</p>
+      <p>Made with {pickRandom(oneliners)}</p>
+    </div>
+    <a href='https://www.contentful.com/' rel='nofollow' target='_blank'>
+      <img
+        className='contentful-attribution'
+        src='https://images.contentful.com/fo9twyrwpveg/44baP9Gtm8qE2Umm8CQwQk/c43325463d1cb5db2ef97fca0788ea55/PoweredByContentful_LightBackground.svg'
+        alt='Powered by Contentful'
+      />
+    </a>
   </footer>
 );
 
