dc9246b1d303629cff44d07c27a8746fed877fb7
Thibault Maekelbergh <thibault.maekelbergh@iCloud.com>
2017-11-27 21:04:16 +0100

    Css fixes for mobile

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-remark`,
+    `gatsby-plugin-offline`,
     {
       resolve: `gatsby-source-contentful`,
       options: {
