9361bdd4b8e3ed1abd80657ab37ea7585f0b7058
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2018-02-19 18:12:23 +0100

    Prep config to read .js files instead

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,8 +1,6 @@
 const path = require(`path`);
 const createPaginatedPages = require(`gatsby-paginate`);
 
-const postTemplate = path.resolve(`./src/templates/post.jsx`);
-
 exports.createPages = ({ graphql, boundActionCreators: { createPage } }) => {
   return new Promise(async (resolve, reject) => {
     try {
@@ -39,7 +37,7 @@ exports.createPages = ({ graphql, boundActionCreators: { createPage } }) => {
       createPaginatedPages({
         edges: data.allContentfulPost.edges,
         createPage: createPage,
-        pageTemplate: `src/templates/index.js`,
+        pageTemplate: `src/templates/Index.js`,
         pageLength: 10, // This is optional and defaults to 10 if not used
         pathPrefix: ``, // This is optional and defaults to an empty string if not used
         context: {}, // This is optional and defaults to an empty object if not used
@@ -47,7 +45,7 @@ exports.createPages = ({ graphql, boundActionCreators: { createPage } }) => {
 
       data.allContentfulPost.edges.forEach(({ node: { id, slug } }) => {
         createPage({
-          component: postTemplate,
+          component: path.resolve(`./src/templates/Post.js`),
           context: {
             id, slug,
           },
