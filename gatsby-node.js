9f82a9f166a716283a4a620ac15edd350275d85a
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2018-02-19 20:17:54 +0100

    Case sensitive bug

--- gatsby-node.js
+++ gatsby-node.js
@@ -37,7 +37,7 @@ exports.createPages = ({ graphql, boundActionCreators: { createPage } }) => {
       createPaginatedPages({
         edges: data.allContentfulPost.edges,
         createPage: createPage,
-        pageTemplate: `src/templates/Index.js`,
+        pageTemplate: `src/templates/index.js`,
         pageLength: 10, // This is optional and defaults to 10 if not used
         pathPrefix: ``, // This is optional and defaults to an empty string if not used
         context: {}, // This is optional and defaults to an empty object if not used
