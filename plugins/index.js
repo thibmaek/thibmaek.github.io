3e7fe76d9ebeec33b90428325d5287843c2687c9
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2018-06-11 21:13:34 +0200

    Add some plugins

---
+++ plugins/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  canonicalUrls: require(`./canonical-urls`),
+  feed: require(`./feed`),
+  sourceContentful: require(`./source-contentful`),
+  transformRemark: require(`./transform-remark`),
+};
