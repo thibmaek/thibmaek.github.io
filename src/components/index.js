a1c49c3c41aab686ee55949cdd37711d36b596ef
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2019-01-23 20:36:54 +0100

    Add a component export mapper

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export * from './comments';
+export * from './footer';
+export * from './header';
+export * from './helmet';
+export * from './nav';
+export * from './post';
+export * from './recent-repos';
