d55380cb4ea1eebe4c72a5ed59b58750b05fcca3
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2019-10-05 00:27:21 +0200

    Configure TS

--- gatsby-site/gatsby-config.js
+++ gatsby-site/gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    'gatsby-plugin-typescript',
     // TODO: include later
     // `gatsby-plugin-no-sourcemaps`,
     // `gatsby-plugin-netlify`,
